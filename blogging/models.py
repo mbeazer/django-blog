831e64f5b3eb31deba1355e29957b1ab9e086a02
mikebeazer <mike@lexicarte.com>
2023-02-11 07:14:28 -0800

    Added blog testing, all tests pass

--- blogging/models.py
+++ blogging/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class Post(models.Model):
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
