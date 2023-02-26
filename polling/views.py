5729276d95c7a4c747ed21b0c6bec5d7e1461f41
mikebeazer <mike@lexicarte.com>
2023-02-26 07:24:41 -0800

    Created class-based view for poll detail

--- polling/views.py
+++ polling/views.py
@@ -3,26 +3,30 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
-
-
-# def list_view(request):
-#     context = {'polls': Poll.objects.all()}
-#     return render(request, 'polling/list.html', context)
-
-
-class ListView:
-    def as_view(self):
-        return self.get
-
-    def get(self, request):
-        model_list_name = self.model.__name__.lower() + '_list'
-        context = {model_list_name: self.model.objects.all()}
-        return render(request, self.template_name, context)
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 class PollListView(ListView):
     model = Poll
     template_name = 'polling/list.html'
+
+
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polling/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
+        if request.POST.get("vote") == "Yes":
+            poll.score += 1
+        else:
+            poll.score -= 1
+        poll.save()
+
+        context = {'object': poll}
+        return render(request, 'polling/detail.html', context)
 
 
 def detail_view(request, poll_id):
