ba2cdfa77c1aca3a3ba427dd19bdcdefdb511478
mikebeazer <mike@lexicarte.com>
2023-02-26 07:39:22 -0800

    Created class-based view for blogging detail

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from blogging.views import BlogListView
-from blogging.views import detail_view
+from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
 ]
