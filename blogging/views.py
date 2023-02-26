8493fc1a77f1231236480bba5f3dda275b8afc47
mikebeazer <mike@lexicarte.com>
2023-02-26 07:35:02 -0800

    Created class-based view for blogging list

--- blogging/views.py
+++ blogging/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
+
+
+class BlogListView(ListView):
+    queryset = Post.objects\
+        .exclude(published_date__exact=None)\
+        .order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
 def detail_view(request, post_id):
